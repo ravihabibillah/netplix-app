91657c01f4be2ee207663462aab5f2edfed7cfff
ravihabibillah <ravihabibillah44@gmail.com>
2024-09-07 08:50:39 +0700

    build: create get it injection

---
+++ lib/injection.dart
@@ -0,0 +1,34 @@
+import 'package:get_it/get_it.dart';
+import 'package:netplix_app/features/movie/data/datasources/movie_remote_data_source.dart';
+import 'package:netplix_app/features/movie/data/repositories/movie_repository_impl.dart';
+import 'package:netplix_app/features/movie/domain/repositories/movie_repository.dart';
+import 'package:netplix_app/features/movie/domain/usecases/get_now_playing.dart';
+import 'package:netplix_app/features/movie/presentation/bloc/now_playing/bloc/now_playing_bloc.dart';
+import 'package:http/http.dart' as http;
+
+final locator = GetIt.instance;
+
+void init() {
+  // BLoC
+  // Now Playing
+  locator.registerFactory(
+    () => NowPlayingBloc(
+      getNowPlaying: locator(),
+    ),
+  );
+
+  // Use Case
+  locator.registerLazySingleton(() => GetNowPlaying(locator()));
+
+  // Repository
+  locator.registerLazySingleton<MovieRepository>(
+    () => MovieRepositoryImpl(remoteDataSource: locator()),
+  );
+
+  // Data Source
+  locator.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(client: locator()));
+
+  // External (HTTP)
+  locator.registerLazySingleton(() => http.Client());
+}
